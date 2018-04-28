c98c49c4dab6fddaecc072302eba0d520b1d90b8
Tomasz Krawczyk <tom_krawczyk@op.pl>
2018-04-28 10:28:02 +0200

    first commit

---
+++ src/main/java/Mysql.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+
+/**
+ * Created by Tomek Krawczyk on 26.04.2018.
+ */
+public class Mysql {
+
+    private static final String DB = "jdbc:mysql://5.135.218.27:3306/spring";
+    private static final String USER = "oskar";
+    private static final String USERPW = "wisla2000";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+
+    public static void main(String[] args) {
+
+
+//        try {
+//            new com.mysql.jdbc.Driver();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+
+        try {
+            Class.forName(DRIVER).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            Connection connection= DriverManager.getConnection(DB,USER,USERPW);
+            Statement statemen=connection.createStatement();
+            ResultSet resultSet=statemen.executeQuery("SELECT * FROM user");
+
+
+
+            while (resultSet.next()){
+
+                System.out.println("--------------------------------------");
+                System.out.println(resultSet.getString("username"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Jeśli wyżej nie ma błedu, to znaczy ze jest połączenie!");
+
+    }
+
+}
